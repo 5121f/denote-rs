740f77609583ee37b73e3aed0919660f995ea7e5
Artyom Kalin <kalin.a.v20031@gmail.com>
2024-05-20 14:25:24 +0300

    update cli

--- src/cli_args.rs
+++ src/cli_args.rs
@@ -4,8 +4,9 @@ use clap::Parser;
 pub(crate) enum Cli {
     Rename {
         file_name: String,
+        #[clap(conflicts_with("date_from_metadata"))]
         date: Option<String>,
-        #[clap(long, short = 'm')]
+        #[clap(long, short = 'm', conflicts_with("date"))]
         date_from_metadata: bool,
     },
     Touch {
