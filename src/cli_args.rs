bd37c00d34c2cf97922bdf0c878ccb9cde534681
Artyom Kalin <kalin.a.v20031@gmail.com>
2024-06-19 10:29:06 +0300

    add description for cli keys

--- src/cli_args.rs
+++ src/cli_args.rs
@@ -5,19 +5,21 @@ pub(crate) enum Cli {
     Rename {
         #[clap(required = true)]
         file_names: Vec<String>,
-        /// You can use "now" to use current time
+        /// Date of file creating. You can use "now" to use current time.
         #[clap(long, short, conflicts_with("date_from_metadata"))]
         date: Option<String>,
+        /// Use date from metadata
         #[clap(long, short = 'm', conflicts_with("date"))]
         date_from_metadata: bool,
         /// Accept all questions
         #[clap(long, short)]
         accept: bool,
+        /// Don't use keywords
         #[clap(long, short = 'k')]
         no_keywords: bool,
     },
     Touch {
-        /// You can use "now" to use current time
+        /// Date of file creating. You can use "now" to use current time.
         #[clap(long, short)]
         date: Option<String>,
     },
