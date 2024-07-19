b95f6f80452154bcc537d4acd336e3a7da65c96f
Artyom Kalin <kalin.a.v20031@gmail.com>
2024-07-19 15:30:01 +0300

    update help message

--- src/cli_args.rs
+++ src/cli_args.rs
@@ -14,7 +14,8 @@ pub(crate) enum Cli {
         #[clap(required = true)]
         file_names: Vec<String>,
         /// The date to be used as the identifier.
-        /// You can use "now" to use current system date time
+        /// Can be used a date like 2022-06-30 or a date and time like 2022-06-16 14:30.
+        /// Can be used "now" to use current system date time.
         #[clap(long, short, conflicts_with("date_from_metadata"))]
         date: Option<String>,
         /// Use creation file date from the file metadata
@@ -44,7 +45,8 @@ pub(crate) enum Cli {
         /// Specify the title
         title: Option<String>,
         /// The date to be used as the identifier.
-        /// You can use "now" to use current system date and time
+        /// Can be used a date like 2022-06-30 or a date and time like 2022-06-16 14:30.
+        /// Can be used "now" to use current system date time.
         #[clap(long, short, default_value = "now")]
         date: String,
         /// Specify the signature
