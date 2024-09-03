7d09c87c953d6e5249cf2ba518b44d478985909f
Artyom Kalin <kalin.a.v20031@gmail.com>
2024-09-03 12:10:18 +0300

    added tests for Extention

--- src/tests/extention.rs
+++ src/tests/extention.rs
@@ -4,5 +4,10 @@
  * file, You can obtain one at https://mozilla.org/MPL/2.0/.
  */
 
-mod extention;
-mod keywords;
+use crate::Extention;
+
+#[test]
+fn test() {
+    assert_eq!(Extention::new(String::from("ext")).to_string(), ".ext");
+    assert_eq!(Extention::new(String::new()).to_string(), "");
+}
