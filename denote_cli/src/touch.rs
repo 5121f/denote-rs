0c2d37d6948638ae6bc4c2b9d9c321ce9705d795
Artyom Kalin <kalin.a.v20031@gmail.com>
2024-10-30 19:12:52 +0300

    refactoring

--- denote_cli/src/touch.rs
+++ denote_cli/src/touch.rs
@@ -13,17 +13,35 @@ use fs_err as fs;
 use crate::{args, ui::UI};
 
 pub fn touch(args: args::Touch, ui: &mut UI) -> anyhow::Result<()> {
-    let denote = build_denote(
-        ui,
-        args.title,
-        args.date,
-        args.signature,
-        args.keywords,
-        args.extension,
-        args.non_interactive,
-    )?;
+    let identifier = Identifier::parse(&args.date).context("Failed to parse identifier")?;
 
-    let file_name = denote.to_string();
+    let interactive = !args.non_interactive;
+
+    let mut name_scheme = Denote::new(identifier);
+
+    if let Some(signature) = args.signature {
+        name_scheme.signature = Signature::parse(&signature)
+    }
+
+    if let Some(title) = args.title {
+        name_scheme.title = Title::parse(&title);
+    } else if interactive {
+        name_scheme.title = ui.take_title()?;
+    }
+
+    if let Some(keywords) = args.keywords {
+        name_scheme.keywords = Keywords::parse_user_input(&keywords);
+    } else if interactive {
+        name_scheme.keywords = ui.take_keywords()?;
+    }
+
+    if let Some(extension) = &args.extension {
+        name_scheme.extension = Extension::new(extension);
+    } else if interactive {
+        name_scheme.extension = ui.take_extension()?;
+    }
+
+    let file_name = name_scheme.to_string();
 
     if !args.accept && !ui.create_file_p(&file_name)? {
         UI::no_action_needed();
@@ -37,46 +55,6 @@ pub fn touch(args: args::Touch, ui: &mut UI) -> anyhow::Result<()> {
     }
 
     Ok(())
-}
-
-fn build_denote(
-    ui: &mut UI,
-    title: Option<String>,
-    date: String,
-    signature: Option<String>,
-    keywords: Option<String>,
-    extension: Option<String>,
-    non_interactive: bool,
-) -> Result<Denote> {
-    let identifier = Identifier::parse(&date).context("Failed to parse identifier")?;
-
-    let interactive = !non_interactive;
-
-    let mut name_scheme = Denote::new(identifier);
-
-    if let Some(signature) = signature {
-        name_scheme.signature = Signature::parse(&signature)
-    }
-
-    if let Some(title) = title {
-        name_scheme.title = Title::parse(&title);
-    } else if interactive {
-        name_scheme.title = ui.take_title()?;
-    }
-
-    if let Some(keywords) = keywords {
-        name_scheme.keywords = Keywords::parse_user_input(&keywords);
-    } else if interactive {
-        name_scheme.keywords = ui.take_keywords()?;
-    }
-
-    if let Some(extension) = &extension {
-        name_scheme.extension = Extension::new(extension);
-    } else if interactive {
-        name_scheme.extension = ui.take_extension()?;
-    }
-
-    Ok(name_scheme)
 }
 
 pub fn open_file(file_name: impl AsRef<Path>) -> Result<()> {
