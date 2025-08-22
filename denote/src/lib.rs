75a7a3942717fa10021bae58aa03b80e1752b944
Artyom Kalin <kalin.a.v20031@gmail.com>
2025-08-22 22:59:24 +0300

    move clippy configs to Cargo.toml

--- denote/src/lib.rs
+++ denote/src/lib.rs
@@ -4,7 +4,6 @@
  * file, You can obtain one at https://mozilla.org/MPL/2.0/.
  */
 
-#![warn(clippy::pedantic)]
 #![allow(clippy::must_use_candidate)]
 #![allow(clippy::missing_panics_doc)]
 #![allow(clippy::missing_errors_doc)]
